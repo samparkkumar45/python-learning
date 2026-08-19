23acf90ebd63911cd1865cfba47ed19b6d110859
samparkkumar45 <samparkkumar2019@gmail.com>
2026-08-19 18:35:29 +0530

    Learn who library works

---
+++ python_dev_understanding/library/module/module_system.py
@@ -0,0 +1,31 @@
+# import my_module as mm 
+# courses = ['Math' , 'English' , 'Arts']
+# index = mm.find_index(courses, 'Math')
+# print(index)
+# print(mm.test)
+# from my_module import * #second method
+# courses = ['Math' , 'English' , 'Arts']
+# index = find_index(courses, 'Math')
+# print(index)
+# print(test)
+
+# import my_module as mm 
+# import sys # to see location list.
+# courses = ['Math' , 'English' , 'Arts']
+# index = mm.find_index(courses, 'Math')
+# print(sys.path)
+# import random
+# courses = ['Math' , 'History' , 'Arts']
+# random_course = random.choice(courses)
+# print(random_course)
+# import math as m
+# rads = m.radians(90)
+# print(m.sin(rads))
+# import datetime
+# import calendar
+# today = datetime.date.today()
+# print(today)
+# print(calendar.isleap(2020))
+import os
+# print(os.getcwd()) # it give current working directory
+print(os.__file__) # give location of file directory
